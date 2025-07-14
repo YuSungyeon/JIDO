bb81fc03acbfaeb2b9b34728386b3942933c33b8
SungYeon <yousung1018@naver.com>
2025-07-15 00:51:02 +0900

    Initial commit_

---
+++ JIDO_/src/test/java/com/goorm/jido_/JidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.jido_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
